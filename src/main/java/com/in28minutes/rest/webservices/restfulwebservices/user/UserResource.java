0f673e924aeb74e201b41718d179fc508d21ad3b
Shakhzodbek <yuldoshov.shakhzod@gmail.com>
2023-10-17 00:25:38 +0200

    Enhanced POST method to return Correct HTTP Status COde and location

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,12 @@ public class UserResource {
 
     // POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
-
 }
